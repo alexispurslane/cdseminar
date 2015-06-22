20ed7c7db4b25bc9ecfc55b364f2b39854238668
Christopher Dumas <christopherdumas@gmail.com>
2015-06-22 08:38:00 -0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  var canvas = document.getElementById('myGame');
+  var ctx = canvas.getContext('2d');
+  
+  // Build a game! (Easier said than done)
+  context.font = '40pt Calibri';
+  context.fillStyle = 'red';
+  context.fillText('Hello World!', canvas.width/2, canvas.height/2);
+}
